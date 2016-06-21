6115696764de5b76798e2412c2a96c3f3a85a50b
Minami Kyohei <kyohei.minami@sacn.co.jp>
2016-06-21 11:55:09 +0900

    プロジェクト作成

---
+++ HotpepperAPIApp/HotpepperAPIApp/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  HotpepperAPIApp
+//
+//  Created by Minami Kyohei on 2016/06/21.
+//  Copyright © 2016年 Minami Kyohei. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
