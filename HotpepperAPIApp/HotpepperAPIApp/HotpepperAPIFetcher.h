4085394c2d5a86948d05eb24f9880eb28d8f5a06
Minami Kyohei <kyohei.minami@sacn.co.jp>
2016-06-21 17:32:25 +0900

    SQLiteでデータベース作成

---
+++ HotpepperAPIApp/HotpepperAPIApp/HotpepperAPIFetcher.h
@@ -0,0 +1,13 @@
+//
+//  HotpepperAPIFetcher.h
+//  HotpepperAPIApp
+//
+//  Created by Minami Kyohei on 2016/06/21.
+//  Copyright © 2016年 Minami Kyohei. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HotpepperAPIFetcher : NSObject
+
+@end
