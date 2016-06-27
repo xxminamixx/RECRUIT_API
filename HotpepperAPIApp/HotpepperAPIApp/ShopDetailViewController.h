e49ed5c8bc3bcbc3c8360a490b69b865a8e432ab
Minami Kyohei <kyohei.minami@sacn.co.jp>
2016-06-27 11:55:27 +0900

    UI配置詳細画面用のクラス作成

---
+++ HotpepperAPIApp/HotpepperAPIApp/ShopDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  ShopDetailViewController.h
+//  HotpepperAPIApp
+//
+//  Created by Minami Kyohei on 2016/06/27.
+//  Copyright © 2016年 Minami Kyohei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ShopDetailViewController : UIViewController
+
+@end
---
+++ HotpepperAPIApp/HotpepperAPIApp/ShopDetailViewController.m
@@ -0,0 +1,37 @@
+//
+//  ShopDetailViewController.m
+//  HotpepperAPIApp
+//
+//  Created by Minami Kyohei on 2016/06/27.
+//  Copyright © 2016年 Minami Kyohei. All rights reserved.
+//
+
+#import "ShopDetailViewController.h"
+
+@interface ShopDetailViewController ()
+
+@end
+
+@implementation ShopDetailViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
