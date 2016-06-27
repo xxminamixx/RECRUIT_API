dcbbe66447e4db00f3dc3d48377666527f0276c0
Minami Kyohei <kyohei.minami@sacn.co.jp>
2016-06-27 15:50:59 +0900

    managerクラス作成

---
+++ HotpepperAPIApp/HotpepperAPIApp/FavoriteShopManager.h
@@ -0,0 +1,13 @@
+//
+//  FavoriteShopManager.h
+//  HotpepperAPIApp
+//
+//  Created by Minami Kyohei on 2016/06/27.
+//  Copyright © 2016年 Minami Kyohei. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FavoriteShopManager : NSObject
+
+@end
---
+++ HotpepperAPIApp/HotpepperAPIApp/FavoriteShopManager.m
@@ -0,0 +1,13 @@
+//
+//  FavoriteShopManager.m
+//  HotpepperAPIApp
+//
+//  Created by Minami Kyohei on 2016/06/27.
+//  Copyright © 2016年 Minami Kyohei. All rights reserved.
+//
+
+#import "FavoriteShopManager.h"
+
+@implementation FavoriteShopManager
+
+@end
