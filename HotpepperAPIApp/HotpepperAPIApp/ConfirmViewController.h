d8624e2c7890305671b2e2c2f2ae942180dccf7d
Minami Kyohei <kyohei.minami@sacn.co.jp>
2016-07-01 17:54:30 +0900

    設定画面、削除確認画面のクラス作成

---
+++ HotpepperAPIApp/HotpepperAPIApp/ConfirmViewController.h
@@ -0,0 +1,13 @@
+//
+//  ConfirmViewController.h
+//  HotpepperAPIApp
+//
+//  Created by Minami Kyohei on 2016/07/01.
+//  Copyright © 2016年 Minami Kyohei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ConfirmViewController : UIViewController
+
+@end
