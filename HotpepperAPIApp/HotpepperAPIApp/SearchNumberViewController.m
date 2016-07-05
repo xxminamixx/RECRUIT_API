32bdb6fe718d7ba620f19351c75e4756c94e3b67
Minami Kyohei <kyohei.minami@sacn.co.jp>
2016-07-05 13:50:58 +0900

    pickerの選択している行数を取得

--- HotpepperAPIApp/HotpepperAPIApp/SearchNumberViewController.m
+++ HotpepperAPIApp/HotpepperAPIApp/SearchNumberViewController.m
@@ -8,6 +8,8 @@
 
 #import "SearchNumberViewController.h"
 
+NSString * const searchNumber = @"SearchNumberSettingKEY";
+
 @interface SearchNumberViewController ()
 @property (weak, nonatomic) IBOutlet UIPickerView *searchNumberPicker;
 
@@ -26,11 +28,13 @@
     // Dispose of any resources that can be recreated.
 }
 
+// 行の数を設定
 -(NSInteger)numberOfComponentsInPickerView:(UIPickerView*)pickerView
 {
     return 1;
 }
 
+// 列の数を設定
 -(NSInteger)pickerView:(UIPickerView*)pickerView numberOfRowsInComponent:(NSInteger)component
 {
     return 10;
@@ -39,6 +43,25 @@
 -(NSString*)pickerView:(UIPickerView*)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component
 {
     return [NSString stringWithFormat:@"%ld", (row + 1) * 10];
+}
+
+// 選択されたピッカービューを取得
+- (void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row inComponent:(NSInteger)component
+{
+    
+    
+    
+    // 0列目の選択している行番号を取得
+    NSInteger selectedRow = ([pickerView selectedRowInComponent:0] + 1) * 10;
+    NSNumber *castRow = [NSNumber numberWithInteger:selectedRow];
+    
+    /*
+    NSUserDefaults *searchNumbserSetting = [NSUserDefaults standardUserDefaults];
+    NSMutableDictionary *savingNumber = [NSMutableDictionary dictionary];
+    [savingNumber setObject:castRow forKey:searchNumber];
+    */
+    
+    NSLog(@"%@", castRow);
 }
 
 
