753881cf749c52d357993fc42985e0c34975a4e4
Minami Kyohei <kyohei.minami@sacn.co.jp>
2016-07-07 17:03:02 +0900

    バージョン取得処理

---
+++ HotpepperAPIApp/HotpepperAPIApp/VersionViewController.m
@@ -0,0 +1,27 @@
+//
+//  VersionViewController.m
+//  HotpepperAPIApp
+//
+//  Created by Minami Kyohei on 2016/07/07.
+//  Copyright © 2016年 Minami Kyohei. All rights reserved.
+//
+
+#import "VersionViewController.h"
+
+@interface VersionViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *version;
+
+@end
+
+@implementation VersionViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.version.text = [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleShortVersionString"];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+@end
