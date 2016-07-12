012f0bee47299932b8847b3aea4c7d2ecc900bd4
Minami Kyohei <kyohei.minami@sacn.co.jp>
2016-07-12 11:14:33 +0900

    ジャンル表示用のセルを追加

---
+++ HotpepperAPIApp/HotpepperAPIApp/ShopGenreTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  ShopGenreTableViewCell.m
+//  HotpepperAPIApp
+//
+//  Created by Minami Kyohei on 2016/07/12.
+//  Copyright © 2016年 Minami Kyohei. All rights reserved.
+//
+
+#import "ShopGenreTableViewCell.h"
+
+@implementation ShopGenreTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
