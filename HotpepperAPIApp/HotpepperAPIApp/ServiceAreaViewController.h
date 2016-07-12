fdc91b1de79fb4cb814f60c760ec5987221a5fc5
Minami Kyohei <kyohei.minami@sacn.co.jp>
2016-07-12 11:18:19 +0900

    使用していないプロトコル削除

--- HotpepperAPIApp/HotpepperAPIApp/ServiceAreaViewController.h
+++ HotpepperAPIApp/HotpepperAPIApp/ServiceAreaViewController.h
@@ -8,13 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@protocol tapCellDelegate <NSObject>
-
-- (void)getIndexPathRow:(NSInteger)index;
-
-@end
-
 @interface ServiceAreaViewController : UIViewController <UITableViewDelegate, UITableViewDataSource>
-@property (weak, nonatomic) id<tapCellDelegate> tapdelegate;
 
 @end
