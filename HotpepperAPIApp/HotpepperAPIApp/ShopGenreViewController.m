d329fc3e9ce8ebca6e0baddac2224480674b8843
Minami Kyohei <kyohei.minami@sacn.co.jp>
2016-07-12 10:35:32 +0900

    ジャンル一覧コントローラー作成

---
+++ HotpepperAPIApp/HotpepperAPIApp/ShopGenreViewController.m
@@ -0,0 +1,22 @@
+//
+//  ShopGenreViewController.m
+//  HotpepperAPIApp
+//
+//  Created by Minami Kyohei on 2016/07/12.
+//  Copyright © 2016年 Minami Kyohei. All rights reserved.
+//
+
+#import "ShopGenreViewController.h"
+
+@implementation ShopGenreViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+
+@end
