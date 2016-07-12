eebdef98ca543eb499f6e1a6365400e5d6994ad0
Minami Kyohei <kyohei.minami@sacn.co.jp>
2016-07-12 09:48:04 +0900

    ジャンル用のEntity作成

---
+++ HotpepperAPIApp/HotpepperAPIApp/ShopGenreEntity.h
@@ -0,0 +1,14 @@
+//
+//  ShopGenreEntity.h
+//  HotpepperAPIApp
+//
+//  Created by Minami Kyohei on 2016/07/12.
+//  Copyright © 2016年 Minami Kyohei. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ShopGenreEntity : NSObject
+@property (nonatomic, strong) NSString *genreCode;
+@property (nonatomic, strong) NSString *genreName;
+@end
---
+++ HotpepperAPIApp/HotpepperAPIApp/ShopGenreEntity.m
@@ -0,0 +1,13 @@
+//
+//  ShopGenreEntity.m
+//  HotpepperAPIApp
+//
+//  Created by Minami Kyohei on 2016/07/12.
+//  Copyright © 2016年 Minami Kyohei. All rights reserved.
+//
+
+#import "ShopGenreEntity.h"
+
+@implementation ShopGenreEntity
+
+@end
